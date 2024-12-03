25239531c90f081e68072660e3d838856c7d4283
Vaidul1s <vaidas.ad25@gmail.com>
2024-12-03 10:13:19 +0200

    12.03

--- santa/app.js
+++ santa/app.js
@@ -35,3 +35,34 @@ btn.addEventListener('click', e => {
 });
 
 console.log(pool);
+
+
+
+  
+// Function to shuffle an array - maiso eiles tvarka del gaveju.
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    console.log(array);
+    return array;       
+};
+
+// Function to assign Secret Santa pairs
+function assignSecretSanta(pool) {
+    const givers = [...pool];
+    let receivers = pool;    
+    while (receivers.some((person, index) => person === givers[index])) {
+        receivers = shuffle([...pool]);
+    };      
+    const pairs = givers.map((giver, index) => ({giver, receiver: receivers[index]}));  
+    return pairs;
+};
+
+const secretSantaPairs = assignSecretSanta(pool);
+
+console.log("Secret Santa Pairs:");
+secretSantaPairs.forEach(pair => {
+    console.log(`${pair.giver} -> ${pair.receiver}`);
+});
