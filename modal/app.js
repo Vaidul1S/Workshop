1278f671c0a33c68da58b904ba580114f0873c5d
Vaidul1s <vaidas.ad25@gmail.com>
2024-12-11 09:17:53 +0200

    12.11

---
+++ modal/app.js
@@ -0,0 +1,18 @@
+console.log('modal');
+
+const btn = document.querySelector('button');
+const modal = document.querySelector('.modal');
+const x = document.querySelector('.close');
+const modalContent = document.querySelector('.modalContent');
+
+btn.addEventListener('click', _ => {
+    modal.style.display = 'block';
+});
+
+x.addEventListener('click', _ => {    
+    modalContent.classList.add('slideUp')
+    setTimeout( _ => {
+        modalContent.classList.remove('slideUp');
+        modal.style.display = 'none';
+    }, 1000);
+});
