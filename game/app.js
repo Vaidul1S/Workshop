52a152fd320d0db263adcb8d7b21115aa18cab8d
Vaidul1s <vaidas.ad25@gmail.com>
2024-12-20 09:11:01 +0200

    12.20

--- game/app.js
+++ game/app.js
@@ -1,6 +1,73 @@
-const cells = document.querySelectorAll('.board div');
+const cell = document.querySelectorAll('.cell');
 const isNaujo = document.querySelector('.new');
-
+const whoGoes = document.getElementById('whoGoes');
+const whoWon = document.getElementById('whoWon');
 let step = '';
 let winner = '';
-let couner = '';
+let counter = '';
+const winCombo = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+const who = _ => {
+    if(step == 'cross') {
+        step = 'circle';
+        whoGoes.innerText = 'Nuliukai';
+    } else {
+        step = 'cross';
+        whoGoes.innerText = 'Kryžiukai';
+    }
+};
+who();
+
+const crossWon = _ => {
+    for (let i = 0; i < winCombo.length; i++) {
+        if (cell[winCombo[i][0]].classList.contains('cross') && cell[winCombo[i][1]].classList.contains('cross') && cell[winCombo[i][2]].classList.contains('cross')) {
+            cell[winCombo[i][0]].classList.add('winColor');
+            cell[winCombo[i][1]].classList.add('winColor');
+            cell[winCombo[i][2]].classList.add('winColor');
+            whoWon.innerText = ' Kryžiukai!';
+        }
+    }
+};
+const circleWon = _ => {
+    for (let i = 0; i < winCombo.length; i++) {
+        if (cell[winCombo[i][0]].classList.contains('circle') && cell[winCombo[i][1]].classList.contains('circle') && cell[winCombo[i][2]].classList.contains('circle')) {
+            cell[winCombo[i][0]].classList.add('winColor');
+            cell[winCombo[i][1]].classList.add('winColor');
+            cell[winCombo[i][2]].classList.add('winColor');
+            whoWon.innerText = ' Nuliukai!';
+        }
+    }
+};
+
+cell.forEach(item => {
+    item.addEventListener('click', _ => {
+        if (!item.classList.contains('circle') && !item.classList.contains('cross')) {
+            item.classList.add(step);
+            if (step == 'cross') {
+                item.innerText = 'X';
+            } else if (step == 'circle') {
+                item.innerText = 'O';
+            }
+            who();
+            crossWon();
+            circleWon();
+        }
+    })
+});
+
+isNaujo.addEventListener('click', _ => {
+    cell.forEach(item => {
+        item.classList.remove('circle', 'cross', 'winColor');
+        item.innerText = ''; 
+        whoWon.innerText = '';              
+    })
+}); 
