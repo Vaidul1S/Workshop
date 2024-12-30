e6f311f319aac2c83f79591d6025d8a62599acf9
Vaidul1s <vaidas.ad25@gmail.com>
2024-12-30 08:29:47 +0200

    12.30

--- slides/app.js
+++ slides/app.js
@@ -1,10 +1,12 @@
 let slideIndex = 0;
 const slides = document.querySelectorAll('.card');
+const dots = document.querySelectorAll('.dot');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
-if (slides.length > 0) {
+if (slides.length > 0 ) {
     slides[slideIndex].classList.add('active');
+    dots[slideIndex].classList.add('activeD');
     slideIndex++
 }
 
@@ -13,12 +15,16 @@ function showSlides(slideIndex) {
         slide.classList.remove('active')
     })
     slides[slideIndex].classList.add('active');
+    dots.forEach((dot, i) =>{
+        dot.classList.remove('activeD')
+    })
+    dots[slideIndex].classList.add('activeD');
 }
-
+   
 setInterval(function(){
     if(slideIndex >= slides.length) {
         slideIndex = 0;
     }
-    showSlides(slideIndex);
+    showSlides(slideIndex);    
     slideIndex++;
 }, 2000);
