a97bdff186d3d63529773535de9d0dc64abfb8a5
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-20 13:10:31 +0300

    08.20

--- secretSanta/app.js
+++ secretSanta/app.js
@@ -69,3 +69,15 @@ btn.addEventListener('click', e => {
         }
     })
 });
+
+function shuffleR(array) {
+    
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+let rev = shuffle(poolRestriction);
+console.log(rev);
