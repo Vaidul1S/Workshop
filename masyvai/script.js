1c8b60008bfe50b70df9db52f10100a0d80d6712
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-22 11:16:23 +0300

    08.22

--- masyvai/script.js
+++ masyvai/script.js
@@ -1,10 +1,13 @@
 console.log('Birbinam............');
 
 let array = [1,2,3,4,5,6,7,8,9,0];
+const section1 = document.querySelector('#section1');
 
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
-    console.log(element);
+    let div = document.createElement('span')
+        div.innerText = array[i];    
+        section1.appendChild(div);
     
 }
 console.log('------------------------------------------------------------');
@@ -12,30 +15,39 @@ console.log('------------------------------------------------------------');
 // Lyginių skaičių radimas sąraše
 // Sukurkite sąrašą skaičių. Naudodami for ciklą ir if sąlygą, išrinkite ir išspausdinkite tik lyginius skaičius iš sąrašo.
 let numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
- 
+const section2 = document.querySelector('#section2');
+
 for (let i = 0; i < numbers1.length; i++) {
     if (numbers1[i] % 2 == 0) {
-        console.log(numbers1[i]);        
+        let div = document.createElement('span')
+        div.innerText = numbers1[i];    
+        section2.appendChild(div);
     }
 }
  
 // Žodžių, ilgesnių nei 5 raidės, spausdinimas
 // Sukurkite sąrašą žodžių. Išspausdinkite tik tuos žodžius, kurių ilgis yra didesnis nei 5 raidės.
 let words = ["apple", "banana", "kiwi", "watermelon", "pear"];
+const section3 = document.querySelector('#section3');
  
 for (let i = 0; i < words.length; i++) {
     if (words[i].length > 5) {
-        console.log(words[i]);        
+        let div = document.createElement('span')
+        div.innerText = words[i];    
+        section3.appendChild(div);         
     }    
 }
  
 // Skaičių dalinimas iš 3 be liekanos
 // Sukurkite sąrašą skaičių nuo 1 iki 20. Išrinkite ir išspausdinkite tik tuos skaičius, kurie dalijasi iš 3 be liekanos.
 let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
- 
+const section4 = document.querySelector('#section4');
+
 for (let i = 0; i < numbers2.length; i++) {
     if (numbers2[i] % 3 == 0) {
-        console.log(numbers2[i]);        
+        let div = document.createElement('span')
+        div.innerText = numbers2[i];    
+        section4.appendChild(div);        
     }    
 }
 console.log('------------------------------------------------------------');
@@ -44,10 +56,13 @@ console.log('------------------------------------------------------------');
 // Neigiamų skaičių suradimas sąraše
 // Sukurkite sąrašą teigiamų ir neigiamų skaičių. Išspausdinkite tik neigiamus skaičius.
 let numbers3 = [4, -3, 2, -1, 0, -5, 8, -9];
- 
+const section5 = document.querySelector('#section5');
+
  for (let i = 0; i < numbers3.length; i++) {
     if (numbers3[i] < 0) {
-        console.log(numbers3[i]);        
+        let div = document.createElement('span')
+        div.innerText = numbers3[i];    
+        section5.appendChild(div);          
     }    
  }
  console.log('------------------------------------------------------------');
