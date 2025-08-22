9f58ea9ec5231505b8eeeead7f0c96aba820ad28
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-22 11:19:35 +0300

    08.22

--- masyvai/script.js
+++ masyvai/script.js
@@ -1,16 +1,15 @@
 console.log('Birbinam............');
 
-let array = [1,2,3,4,5,6,7,8,9,0];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const section1 = document.querySelector('#section1');
 
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
     let div = document.createElement('span')
-        div.innerText = array[i];    
-        section1.appendChild(div);
-    
+    div.innerText = array[i];
+    section1.appendChild(div);
+
 }
-console.log('------------------------------------------------------------');
 
 // Lyginių skaičių radimas sąraše
 // Sukurkite sąrašą skaičių. Naudodami for ciklą ir if sąlygą, išrinkite ir išspausdinkite tik lyginius skaičius iš sąrašo.
@@ -20,24 +19,24 @@ const section2 = document.querySelector('#section2');
 for (let i = 0; i < numbers1.length; i++) {
     if (numbers1[i] % 2 == 0) {
         let div = document.createElement('span')
-        div.innerText = numbers1[i];    
+        div.innerText = numbers1[i];
         section2.appendChild(div);
     }
 }
- 
+
 // Žodžių, ilgesnių nei 5 raidės, spausdinimas
 // Sukurkite sąrašą žodžių. Išspausdinkite tik tuos žodžius, kurių ilgis yra didesnis nei 5 raidės.
 let words = ["apple", "banana", "kiwi", "watermelon", "pear"];
 const section3 = document.querySelector('#section3');
- 
+
 for (let i = 0; i < words.length; i++) {
     if (words[i].length > 5) {
         let div = document.createElement('span')
-        div.innerText = words[i];    
-        section3.appendChild(div);         
-    }    
+        div.innerText = words[i];
+        section3.appendChild(div);
+    }
 }
- 
+
 // Skaičių dalinimas iš 3 be liekanos
 // Sukurkite sąrašą skaičių nuo 1 iki 20. Išrinkite ir išspausdinkite tik tuos skaičius, kurie dalijasi iš 3 be liekanos.
 let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
@@ -46,61 +45,63 @@ const section4 = document.querySelector('#section4');
 for (let i = 0; i < numbers2.length; i++) {
     if (numbers2[i] % 3 == 0) {
         let div = document.createElement('span')
-        div.innerText = numbers2[i];    
-        section4.appendChild(div);        
-    }    
+        div.innerText = numbers2[i];
+        section4.appendChild(div);
+    }
 }
-console.log('------------------------------------------------------------');
 
- 
 // Neigiamų skaičių suradimas sąraše
 // Sukurkite sąrašą teigiamų ir neigiamų skaičių. Išspausdinkite tik neigiamus skaičius.
 let numbers3 = [4, -3, 2, -1, 0, -5, 8, -9];
 const section5 = document.querySelector('#section5');
 
- for (let i = 0; i < numbers3.length; i++) {
+for (let i = 0; i < numbers3.length; i++) {
     if (numbers3[i] < 0) {
         let div = document.createElement('span')
-        div.innerText = numbers3[i];    
-        section5.appendChild(div);          
-    }    
- }
- console.log('------------------------------------------------------------');
+        div.innerText = numbers3[i];
+        section5.appendChild(div);
+    }
+}
 
 // Teksto paieška sąraše
 // Sukurkite sąrašą sakinių. Išspausdinkite tik tuos sakinius, kurie turi žodį „JavaScript“.
 let sentences = ["I love programming", "JavaScript is awesome", "Coding is fun", "Learn JavaScript"];
+const section6 = document.querySelector('#section6');
 
-for (let i = 0; i <sentences.length; i++) {
+for (let i = 0; i < sentences.length; i++) {
     if (sentences[i].includes('JavaScript')) {
-        console.log(sentences[i]);        
-    }    
+        let div = document.createElement('span')
+        div.innerText = sentences[i];
+        section6.appendChild(div);
+    }
 }
-console.log('------------------------------------------------------------');
 
 // Skaičių kvadratų suradimas
 // Sukurkite sąrašą skaičių. Išspausdinkite tik tuos skaičius, kurių kvadratas yra didesnis nei 50.
 let numbers4 = [2, 5, 7, 8, 10, 12];
- 
- for (let i = 0; i < numbers4.length; i++) {
-    if (numbers4[i]*numbers4[i] > 50) {
-        console.log(numbers4[i]);        
+const section7 = document.querySelector('#section7');
+
+for (let i = 0; i < numbers4.length; i++) {
+    if (numbers4[i] * numbers4[i] > 50) {
+        let div = document.createElement('span')
+        div.innerText = numbers4[i];
+        section7.appendChild(div);
     }
- }
- console.log('------------------------------------------------------------');
+}
+console.log('------------------------------------------------------------');
 
 // Didžiausio skaičiaus paieška sąraše
 // Sukurkite sąrašą skaičių ir raskite didžiausią skaičių. Tikrinkite kiekvieną reikšmę cikle.
 let numbers5 = [3, 45, 23, 67, 12, 90, 34];
 let didziausias = 0;
 
- for (let i = 0; i < numbers5.length; i++) {    
+for (let i = 0; i < numbers5.length; i++) {
     if (numbers5[i] > didziausias) {
         didziausias = numbers5[i];
-    }       
- }
- console.log(didziausias); 
- console.log('------------------------------------------------------------');
+    }
+}
+console.log(didziausias);
+console.log('------------------------------------------------------------');
 
 // Raidžių skaičiavimas žodyje
 // Sukurkite žodį ir suskaičiuokite, kiek kartų žodyje pasikartoja raidė „a“.
@@ -109,8 +110,8 @@ let countA = 0;
 
 for (let i = 0; i < word.length; i++) {
     if (word[i].includes('a')) {
-        countA +=1;
-    }    
+        countA += 1;
+    }
 }
 console.log(countA);
 console.log('------------------------------------------------------------');
@@ -122,7 +123,7 @@ let pilnaprociai = 0;
 
 for (let i = 0; i < ages.length; i++) {
     if (ages[i] > 18) {
-        console.log(ages[i]);        
+        console.log(ages[i]);
     }
 }
 console.log('------------------------------------------------------------');
@@ -133,6 +134,6 @@ let numbers6 = [5, 8, 13, 22, 31, 40];
 
 for (let i = 0; i < numbers6.length; i++) {
     if (numbers6[i] % 2) {
-        console.log(numbers6[i]);        
+        console.log(numbers6[i]);
     }
 }
